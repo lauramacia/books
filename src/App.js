c7677032247ba185db559ef5287e8e37a66550a8
Laura Macia <LMacia@ConnectWise.com>
2025-02-21 15:55:22 -0500

    Context Refactor complete

--- src/App.js
+++ src/App.js
@@ -1,66 +1,20 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import { useEffect, useContext } from 'react';
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
+import BooksContext from './context/books';
 
-function App() {
-  const [books, setBooks] = useState([]);
-
-  const fetchBooks = async () => {
-    const response = await axios.get('http://localhost:3001/books');
-
-    setBooks(response.data);
-  };
-
-  //useEffect is a hook that runs either only on FIRST render of the component, or at VERY SPECIFIC times when a variable in the dependency array changes
-  // with second argument [], this is only called on first render and NEVER again
-  // with NO second argument, this is called on first render and EVERY render
-  // with a second argument [books], this is called on first render and EVERY render where the value of books changes (or any other variable defined in the app)
+function App() {  
+  const { fetchBooks } = useContext(BooksContext);
+  
   useEffect(() => {
     fetchBooks();
-  }, []);
-
-  const editBookById = async (id, newTitle) => {
-    const response = await axios.put('http://localhost:3001/books/' + id, {
-      title: newTitle,
-    });
-
-    const updatedBooks = books.map((book) => {
-      if (book.id === id) {
-        // by returning response instead of only changed value, we will capture any other changes that occurred outside of this process (e.g. someone else changing the book)
-        return { ...book, ...response.data };
-      }
-
-      return book;
-    });
-
-    setBooks(updatedBooks);
-  };
-
-  const deleteBookById = async (id) => {
-    await axios.delete('http://localhost:3001/books/' + id);
-
-    const updatedBooks = books.filter((book) => {
-      return book.id !== id;
-    });
-
-    setBooks(updatedBooks);
-  };
-
-  const createBook = async (title) => {
-    const response = await axios.post('http://localhost:3001/books', {
-      title,
-    });
-
-    const updatedBooks = [...books, response.data];
-    setBooks(updatedBooks);
-  };
+  }, []); 
 
   return (
     <div className="app">
       <h1>Reading List</h1>
-      <BookList onEdit={editBookById} books={books} onDelete={deleteBookById} />
-      <BookCreate onCreate={createBook} />
+      <BookList />
+      <BookCreate />
     </div>
   );
 }
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,10 +1,12 @@
-import { useContext } from "react";
-import BooksContext from "../context/books";
+import useBooksContext from "../hooks/use-books-context";
 import BookShow from "./BookShow";
 
-function BookList( {books, onDelete, onEdit}) {
-    const renderedBooks = books.map((book) => {
-    return <BookShow key={book.id} book={book} onDelete={onDelete} onEdit={onEdit}/>;
+
+function BookList() {
+  const { books } = useBooksContext();  
+
+  const renderedBooks = books?.map((book) => {
+    return <BookShow key={book.id} book={book} />;
   });
 
   return <div className="book-list">
